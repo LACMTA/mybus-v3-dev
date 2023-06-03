7341c9aa72a3d17e226e6d48afcef55d07dae9ad
Nina Kin <matikin9@gmail.com>
2023-06-03 15:30:57 -0700

    finish i18n handling

--- process-data.js
+++ process-data.js
@@ -6,7 +6,7 @@ const INPUT_FILE = 'src/_data/june-2023-translated.csv';
 const OUTPUT_PATH = 'src/data/';
 const file = fs.createReadStream(INPUT_FILE);
 let count = 0;
-const langs = ['en', 'es'];
+const langs = ['en', 'es', 'hy', 'ja', 'ko', 'ru', 'vi', 'zh-tw'];
 let translation_results = {
     'lines': [],
     'en': {
@@ -15,6 +15,30 @@ let translation_results = {
     },
     'es': {
         'lang': 'es',
+        'updates': {}
+    },
+    'hy': {
+        'lang': 'hy',
+        'updates': {}
+    },
+    'ja': {
+        'lang': 'ja',
+        'updates': {}
+    },
+    'ko': {
+        'lang': 'ko',
+        'updates': {}
+    },
+    'ru': {
+        'lang': 'ru',
+        'updates': {}
+    },
+    'vi': {
+        'lang': 'vi',
+        'updates': {}
+    },
+    'zh-tw': {
+        'lang': 'zh-tw',
         'updates': {}
     }
 };
@@ -26,10 +50,12 @@ papa.parse(file, {
     complete: function(results) {
         try {
             results.data.forEach(function(item) {
-                if (!translation_results.lines.includes(item.line)) {
-                    translation_results.lines.push(item.line);
+                let line = item.line;
+                
+                if (!translation_results.lines.includes(line)) {
+                    translation_results.lines.push(line);
                     langs.forEach(function(lang) {
-                        translation_results[lang].updates[item.line] = [];
+                        translation_results[lang].updates[line] = [];
                     });
                 }
                 langs.forEach(function(lang) {
@@ -37,7 +63,7 @@ papa.parse(file, {
                     update['update'] = item[lang + '-update'];
                     update['reason'] = item[lang + '-reason'];
                     update['map'] = item['map'];
-                    translation_results[lang].updates[item.line].push(update);
+                    translation_results[lang].updates[line].push(update);
                 });
             });
             // console.log(results);
