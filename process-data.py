77ebbf0f34a12b6d75b842849bc64c426369f0be
Nina Kin <matikin9@gmail.com>
2023-06-09 14:05:02 -0700

    clean up files

---
+++ process-data.py
@@ -0,0 +1,57 @@
+import pandas as pd
+
+SOURCE_FILE = 'src/_data/june-2023-translated-final.xlsx'
+
+LINES_OUTPUT = 'src/data/lines.json'
+UPDATES_OUTPUT = 'src/_data/updates.csv'
+CONTENT_OUTPUT = 'src/_data/content.csv'
+
+print("Running process-data.py\n")
+
+# need to run pip3 install openpyxl
+try:
+    updates_df = pd.read_excel(SOURCE_FILE, 'updates', engine = 'openpyxl')
+    print("Read \"updates\" sheet from:", SOURCE_FILE)
+except Exception as e:
+    print("Couldn't read \"updates\" sheet from:", SOURCE_FILE)
+    print(str(e))
+
+try:
+    lines_df = pd.read_excel(SOURCE_FILE, 'lines', engine = 'openpyxl', 
+                        dtype={ 'line_label':str })
+    print("Read \"lines\" sheet from:", SOURCE_FILE)
+except Exception as e:
+    print("Couldn't read \"lines\" sheet from:", SOURCE_FILE)
+    print(str(e))
+
+try:
+    content_df = pd.read_excel(SOURCE_FILE, 'content', engine = 'openpyxl')
+    print("Read \"content\" sheet from:", SOURCE_FILE)
+except Exception as e:
+    print("Couldn't read \"content\" sheet from:", SOURCE_FILE)
+    print(str(e))
+
+print("")
+
+try:
+    lines_df.to_json(LINES_OUTPUT, orient='records')
+    print("Wrote to:", LINES_OUTPUT)
+except Exception as e:
+    print("Couldn't write to:", LINES_OUTPUT)
+    print(str(e))
+
+try:
+    updates_df.to_csv(UPDATES_OUTPUT)
+    print("Wrote to:", UPDATES_OUTPUT)
+except Exception as e:
+    print("Couldn't write to:", UPDATES_OUTPUT)
+    print(str(e))
+
+try:
+    content_df.to_csv(CONTENT_OUTPUT)
+    print("Wrote to:", CONTENT_OUTPUT)
+except Exception as e:
+    print("Couldn't write to:", CONTENT_OUTPUT)
+    print(str(e))
+
+print("\n")
